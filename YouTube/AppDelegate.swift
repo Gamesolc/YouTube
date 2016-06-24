66205d3cf1d684f798c4989442ae889692bd54be
Liam Mendes <liammendes@Liams-MacBook-Pro.local>
2016-06-24 12:45:02 -0700

    Initial Commit

---
+++ YouTube/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  YouTube
+//
+//  Created by Liam Mendes on 24/06/2016.
+//  Copyright © 2016 Liam Mendes. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ YouTube/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  YouTube
+//
+//  Created by Liam Mendes on 24/06/2016.
+//  Copyright © 2016 Liam Mendes. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
